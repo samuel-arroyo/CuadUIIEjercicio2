9aa02f0ca5a755a01c575940afecc7f11e9fff2e
Samuel Arroyo <42401837@uaz.edu.mx>
2025-10-28 18:39:04 +0100

    Bienvenido al mundo Java.

---
+++ src/cuaduiiejercicio2/CuadUIIEjercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuaduiiejercicio2;
+
+/**
+ *
+ * @author Samuel A
+ */
+public class CuadUIIEjercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Bienvenido al mundo Java.");
+    }
+    
+}
